429382ed6cf8720c9f73728cd0275e878000f501
steve <stevemberry2@gmail.com>
2024-01-03 11:26:25 -0600

    added arrays for password content and created generate password function

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // Assignment code here
+ var characterLength = 12;
+ var choiceArr = [];
 
+ var specialCharArr = ['!','?','@','#','$','%'];
+ var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+ var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+ var numberArr = ['0','1','2','3','4','5','6','7','8','9'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +23,14 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+//Generate a password according to prompt
+function getPrompts(){
+  characterLength = parseInt(prompt('How many characters should password be? Password should be between 12 - 128 characters long.'));
+  
+  //Check for password length parameters
+     
+}
+}
+
+
